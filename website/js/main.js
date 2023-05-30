6004c4617f1b1d0393797cf0fdbccc5624af4c0c
Zark100 <135060972+Zark100@users.noreply.github.com>
2023-05-30 06:46:54 -1200

    Add files via upload

---
+++ website/js/main.js
@@ -0,0 +1,6 @@
+const seconds = document.querySelector('seconds number');
+minutes = document.querySelector(".minutes .number");
+hours = document.querySelector(".hours .number");
+days = document.querySelector(".days .number");
+
+console.log(seconds, minutes, hours, days);
